1b2b90e55ec520bebc31a5c075521be05d19fcce
Lee Seaver <lee.seaver@gmail.com>
2023-08-15 15:50:14 -0700

    deployment fixing 4

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,6 +1,6 @@
+import Replicate from "replicate";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-import Replicate from "replicate";
 
 import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
 import { checkSubscription } from "@/lib/subscription";
@@ -8,11 +8,14 @@ import { checkSubscription } from "@/lib/subscription";
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN!,
 });
-export async function POST(req: Request) {
+
+export async function POST(
+  req: Request
+) {
   try {
     const { userId } = auth();
     const body = await req.json();
-    const { prompt } = body;
+    const { prompt  } = body;
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
@@ -21,12 +24,14 @@ export async function POST(req: Request) {
     if (!prompt) {
       return new NextResponse("Prompt is required", { status: 400 });
     }
+
     const freeTrial = await checkApiLimit();
     const isPro = await checkSubscription();
 
     if (!freeTrial && !isPro) {
-      return new NextResponse("Free trial has expired", { status: 403 });
+      return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
     }
+
     const response = await replicate.run(
       "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
       {
@@ -36,12 +41,12 @@ export async function POST(req: Request) {
       }
     );
     if (!isPro) {
-      // dont forget to increase the api limit  
       await increaseApiLimit();
     }
+
     return NextResponse.json(response);
   } catch (error) {
-    console.log("[VIDEO_ERROR]", error);
-    return new NextResponse("Internal error", { status: 500 });
+    console.log('[VIDEO_ERROR]', error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
-}
+};
