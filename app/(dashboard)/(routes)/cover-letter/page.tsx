99d9087f813457955050561095c6bee3e2a7b6be
Lee Seaver <lee.seaver@gmail.com>
2023-08-16 13:26:33 -0700

    Cover Letter Generator Completed pt1

--- app/(dashboard)/(routes)/cover-letter/page.tsx
+++ app/(dashboard)/(routes)/cover-letter/page.tsx
@@ -19,12 +19,10 @@ import { useState } from "react";
 
 import { useProModal } from "@/hooks/use-pro-modal";
 
-
-
 const ConverLetterGeneratorPage = () => {
   const proModal = useProModal();
   const router = useRouter();
-  const [generatedCoverLetter, setGeneratedCoverLetter] = useState("")
+  const [generatedCoverLetter, setGeneratedCoverLetter] = useState("");
   const [isEditing, setIsEditing] = useState(false); // State to track edit mode
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -38,10 +36,12 @@ const ConverLetterGeneratorPage = () => {
   const isLoading = form.formState.isSubmitting;
 
   const downloadCoverLetter = () => {
-    const fileName = `CoverLetter_${new Date().toISOString().slice(0, 10)}_${generatedCoverLetter.slice(0, 10)}.txt`;
-    const blob = new Blob([generatedCoverLetter], { type: 'text/plain' });
+    const fileName = `CoverLetter_${new Date()
+      .toISOString()
+      .slice(0, 10)}_${generatedCoverLetter.slice(0, 10)}.txt`;
+    const blob = new Blob([generatedCoverLetter], { type: "text/plain" });
     const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
+    const a = document.createElement("a");
     a.href = url;
     a.download = fileName;
     document.body.appendChild(a);
@@ -50,17 +50,16 @@ const ConverLetterGeneratorPage = () => {
     window.URL.revokeObjectURL(url);
   };
 
-
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      const response = await axios.post('/api/cvg', values);
+      const response = await axios.post("/api/cvg", values);
       setGeneratedCoverLetter(response.data.coverLetter);
       toast.success("Cover letter generated successfully!");
     } catch (error: any) {
       if (error?.response?.status === 403) {
         proModal.onOpen();
       } else {
-        toast.error("Something went wrong")
+        toast.error("Something went wrong");
       }
     } finally {
       router.refresh();
@@ -88,7 +87,7 @@ const ConverLetterGeneratorPage = () => {
         iconColor="text-teal-300"
         bgColor="bg-teal-500/10"
       />
-      <div className="px-4 lg:px-8">
+        <div className="px-4 lg:px-8">
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
@@ -98,13 +97,16 @@ const ConverLetterGeneratorPage = () => {
               name="resume"
               render={({ field }) => (
                 <FormItem className="mb-4">
-                  <FormControl label="Your Resume">
-                    <textarea
-                      className="resize-vertical border rounded-md p-2 w-full"
-                      placeholder="Paste your resume here"
-                      {...field}
-                      rows={10}
-                    />
+                  <FormControl>
+                    <div>
+                      <label className="block mb-2">Your Resume</label>
+                      <textarea
+                        className="resize-vertical border rounded-md p-2 w-full"
+                        placeholder="Paste your resume here"
+                        {...field}
+                        rows={10}
+                      />
+                    </div>
                   </FormControl>
                 </FormItem>
               )}
@@ -113,13 +115,16 @@ const ConverLetterGeneratorPage = () => {
               name="jobDescription"
               render={({ field }) => (
                 <FormItem className="mb-2">
-                  <FormControl label="Job Description">
-                    <textarea
-                      className="resize-vertical border rounded-md p-2 w-full"
-                      placeholder="Paste the job description here"
-                      {...field}
-                      rows={10}
-                    />
+                  <FormControl>
+                    <div>
+                      <label className="block mb-2">Job Description</label>
+                      <textarea
+                        className="resize-vertical border rounded-md p-2 w-full"
+                        placeholder="Paste the job description here"
+                        {...field}
+                        rows={10}
+                      />
+                    </div>
                   </FormControl>
                 </FormItem>
               )}
@@ -141,43 +146,42 @@ const ConverLetterGeneratorPage = () => {
         )}
       </div>
       <div className="px-4 lg:px-8">
-
-      <div className="p-8 pb-3 mt-8 border rounded-lg">
-        <h3 className="text-xl font-bold mb-4">Generated Cover Letter:</h3>
-        {isEditing ? ( // If in edit mode, show the textarea
-          <textarea
-            className="resize-vertical border rounded-md p-2 w-full bg-gray-100"
-            value={generatedCoverLetter}
-            onChange={(e) => setGeneratedCoverLetter(e.target.value)}
-            rows={15}
-          />
-        ) : (
-          // If not in edit mode, show the preformatted cover letter
-          <pre className="whitespace-pre-wrap bg-gray-100 p-4 rounded-md border">
-            {generatedCoverLetter}
-          </pre>
-        )}
-        <div className="flex justify-start mt-2">
-          <Button onClick={handleEditToggle}>
-            {isEditing ? "Cancel" : "Edit Cover Letter"}
-          </Button>
-          {isEditing && (
-            <Button
-              onClick={handleEditToggle}
-              className="bg-green-500 hover:bg-green-600 ml-4"
-            >
-              Save
-            </Button>
+        <div className="p-8 pb-3 mt-8 border rounded-lg">
+          <h3 className="text-xl font-bold mb-4">Generated Cover Letter:</h3>
+          {isEditing ? ( // If in edit mode, show the textarea
+            <textarea
+              className="resize-vertical border rounded-md p-2 w-full bg-gray-100"
+              value={generatedCoverLetter}
+              onChange={(e) => setGeneratedCoverLetter(e.target.value)}
+              rows={15}
+            />
+          ) : (
+            // If not in edit mode, show the preformatted cover letter
+            <pre className="whitespace-pre-wrap bg-gray-100 p-4 rounded-md border">
+              {generatedCoverLetter}
+            </pre>
           )}
+          <div className="flex justify-start mt-2">
+            <Button onClick={handleEditToggle}>
+              {isEditing ? "Cancel" : "Edit Cover Letter"}
+            </Button>
+            {isEditing && (
+              <Button
+                onClick={handleEditToggle}
+                className="bg-green-500 hover:bg-green-600 ml-4"
+              >
+                Save
+              </Button>
+            )}
             <Button
               onClick={downloadCoverLetter}
               className="bg-blue-500 hover:bg-blue-600 ml-4"
             >
               Download Cover Letter
             </Button>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
